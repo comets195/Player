7fd1479dadbd0bdcc9128fc48a6b6e4f4c603753
Leedongjin-nbt <lee.dongjin@nbt.com>
2021-07-01 16:23:14 +0900

    추가: 앨범리스트 Cell UI구현

---
+++ Player/Sources/View/AlbumCollectionViewCell.swift
@@ -0,0 +1,91 @@
+//
+//  AlbumCollectionViewCell.swift
+//  Player
+//
+//  Created by DongJin Lee on 2021/07/01.
+//
+
+import UIKit
+
+final class AlbumCollectionViewCell: UICollectionViewCell {
+    static let identifier = String(describing: type(of: self))
+    
+    struct Constant {
+        static let leftPadding: CGFloat = 8
+        static let rightPadding: CGFloat = -8
+        static let width = (UIScreen.main.bounds.width - (StageViewController.Constant.insetSpacing * 3)) / 2
+    }
+    
+    private var artwork = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.backgroundColor = .yellow
+    }
+    
+    private var title = UILabel().then {
+        $0.font = UIFont.boldSystemFont(ofSize: 14.0)
+        $0.textColor = .black
+        $0.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        $0.text = "Album Title"
+    }
+    
+    private var artist = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 12.0)
+        $0.textColor = .gray
+        $0.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        $0.text = "Artist"
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.layer.cornerRadius = 16.0
+        contentView.layer.masksToBounds = true
+        
+        layer.shadowPath = UIBezierPath(rect: contentView.bounds).cgPath
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+        layer.shadowRadius = 5.0
+        layer.shadowOpacity = 0.1
+        layer.masksToBounds = false
+    }
+    
+    private func configureUI() {
+        contentView.backgroundColor = .white
+        backgroundColor = .clear
+        contentView.addSubview(artwork)
+        contentView.addSubview(title)
+        contentView.addSubview(artist)
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        artwork.snp.makeConstraints { make in
+            make.top.equalTo(snp.top)
+            make.left.equalTo(snp.left)
+            make.right.equalTo(snp.right)
+            make.width.equalTo(Constant.width)
+        }
+        
+        title.snp.makeConstraints { make in
+            make.top.equalTo(artwork.snp.bottom).offset(6)
+            make.left.equalTo(artwork).offset(Constant.leftPadding)
+            make.right.equalTo(artwork).offset(Constant.rightPadding)
+        }
+        
+        artist.snp.makeConstraints { make in
+            make.top.equalTo(title.snp.bottom).offset(4)
+            make.left.equalTo(title)
+            make.right.equalTo(title)
+            make.bottom.equalTo(snp.bottom)
+        }
+    }
+}
--- Player/Sources/ViewController/StageViewController.swift
+++ Player/Sources/ViewController/StageViewController.swift
@@ -8,18 +8,53 @@
 import UIKit
 
 final class StageViewController: UIViewController {
-
+    
+    struct Constant {
+        static let cellHeightRatio: CGFloat = 1.1
+        static let itemOutSpacing: CGFloat = 14.0
+        static let insetSpacing: CGFloat = 14.0
+    }
+    
+    private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        let layer = UICollectionViewFlowLayout()
+        layer.scrollDirection = .vertical
+        
+        $0.collectionViewLayout = layer
+        $0.backgroundColor = .white
+        $0.contentInset = UIEdgeInsets(top: Constant.insetSpacing,
+                                       left: Constant.insetSpacing,
+                                       bottom: Constant.insetSpacing,
+                                       right: Constant.insetSpacing)
+    }
+    
     private var viewModel: StageViewModelType = StageViewModel()
     
     override func loadView() {
         super.loadView()
-        view.backgroundColor = .yellow
-        
+        view.backgroundColor = .white
+        configureUI()
+        configureConstraints()
         bind()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    private func configureUI() {
+        view.addSubview(collectionView)
+        collectionView.register(AlbumCollectionViewCell.self, forCellWithReuseIdentifier: AlbumCollectionViewCell.identifier)
+    }
+    
+    private func configureConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.bottom.equalTo(view.snp.bottom)
+        }
     }
     
     private func bind() {
@@ -32,5 +67,52 @@ final class StageViewController: UIViewController {
         viewModel.output.pushSongList.bind { [weak self] album in
             print(album)
         }
+    }
+}
+
+// MARK: - DataSource Methods.
+extension StageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+    
+
+}
+
+// MARK: - Delegate Methods.
+extension StageViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+
+// MARK: - FlowLayout Methods.
+extension StageViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: AlbumCollectionViewCell.Constant.width,
+                      height: AlbumCollectionViewCell.Constant.width * Constant.cellHeightRatio)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return Constant.itemOutSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return Constant.itemOutSpacing
     }
 }
