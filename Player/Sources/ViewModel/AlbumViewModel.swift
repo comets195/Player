02044affd2c116d48c3ce712816848a75e897e4f
Dong Jin Lee <lee.dongjin@nbt.com>
2021-07-02 21:50:56 +0900

    추가: 앨범 ViewModel

--- Player/Sources/ViewModel/AlbumViewModel.swift
+++ Player/Sources/ViewModel/AlbumViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import MediaPlayer
+
+protocol AlbumViewModelType {
+    var input: AlbumViewModelInput { get }
+    var output: AlbumViewModelOutput { get }
+    
+    var album: Album? { get }
+    var songs: [MPMediaItem]? { get }
+}
+
+protocol AlbumViewModelInput {
+    var headerViewAlbum: State<Void> { get }
+}
+
+protocol AlbumViewModelOutput {
+    
+}
+
+final class AlbumViewModel: AlbumViewModelType {
+    struct Input: AlbumViewModelInput {
+        var headerViewAlbum = State<Void>(nil)
+    }
+    
+    struct Output: AlbumViewModelOutput {
+        
+    }
+    
+    var input: AlbumViewModelInput = Input()
+    var output: AlbumViewModelOutput = Output()
+    var album: Album?
+    var songs: [MPMediaItem]?
+    
+    init(album: Album, songs: [MPMediaItem]) {
+        self.album = album
+        self.songs = songs
+        bind()
+    }
+    
+    private func bind() {
+        
+    }
+}
