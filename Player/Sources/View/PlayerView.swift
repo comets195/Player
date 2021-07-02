dbb76d4e42d007d58c01a443778683716e6f41e4
Leedongjin-nbt <lee.dongjin@nbt.com>
2021-07-02 18:33:17 +0900

    추가: player View 버튼추가

--- Player/Sources/View/PlayerView.swift
+++ Player/Sources/View/PlayerView.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import MediaPlayer
 
 final class PlayerView: UIView {
 
+    struct Constant {
+        static let progressHeight: CGFloat = 4
+    }
+    
     private var progressView = UIProgressView().then {
         $0.tintColor = .black
         $0.progress = 0.5
@@ -16,7 +21,6 @@ final class PlayerView: UIView {
     
     private var artwork = UIImageView().then {
         $0.contentMode = .scaleAspectFill
-        $0.layer.cornerRadius = 8.0
         $0.backgroundColor = .black
     }
     
@@ -36,10 +40,48 @@ final class PlayerView: UIView {
         $0.textAlignment = .center
     }
     
-    private var playControlButton = UIButton().then {
+    private(set) var playControlButton = UIButton().then {
         $0.setImage(UIImage(systemName: "play"), for: .normal)
         $0.setImage(UIImage(systemName: "pause"), for: .selected)
         $0.tintColor = .black
+    }
+    
+    private(set) var loopButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "shuffle"), for: .normal)
+        $0.tintColor = .black
+    }
+    
+    private(set) var shuffleButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "shuffle"), for: .normal)
+        $0.tintColor = .black
+        $0.isHidden = true
+    }
+    
+    private(set) var rewindButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "shuffle"), for: .normal)
+        $0.tintColor = .black
+        $0.isHidden = true
+    }
+    
+    private(set) var fastForwardButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "shuffle"), for: .normal)
+        $0.tintColor = .black
+        $0.isHidden = true
+    }
+    
+    private(set) var volumeSlider = MPVolumeView().then {
+        $0.tintColor = .black
+        $0.isHidden = true
+        guard let routeButton = $0.subviews.last as? UIButton else { return }
+        let image = routeButton.currentImage?.withRenderingMode(.alwaysTemplate)
+        $0.setRouteButtonImage(image, for: .normal)
+    }
+    
+    private(set) var remainTime = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 10.0)
+        $0.textColor = .gray
+        $0.text = "00:00"
+        $0.isHidden = true
     }
     
     override init(frame: CGRect) {
@@ -64,23 +106,52 @@ final class PlayerView: UIView {
         let blurEffect = UIBlurEffect(style: .regular)
         let visualEffect = UIVisualEffectView(effect: blurEffect)
         visualEffect.frame = bounds
-
-        addSubview(visualEffect)
         clipsToBounds = true
         
+        addSubview(visualEffect)
         addSubview(progressView)
         addSubview(artwork)
         addSubview(title)
         addSubview(artistAndSong)
         addSubview(playControlButton)
+        addSubview(loopButton)
+        addSubview(shuffleButton)
+        addSubview(rewindButton)
+        addSubview(fastForwardButton)
+        addSubview(volumeSlider)
+        addSubview(remainTime)
     }
     
     private func configureConstraint() {
+        title.snp.makeConstraints { make in
+            make.top.equalTo(snp.top).offset(15 + Constant.progressHeight)
+            make.right.equalTo(snp.right).offset(-(15 + 50))
+            make.left.equalTo(snp.left).offset(15 + 50)
+        }
+        
+        artistAndSong.snp.makeConstraints { make in
+            make.top.equalTo(title.snp.bottom)
+            make.left.equalTo(title.snp.left)
+            make.right.equalTo(title.snp.right)
+        }
+        
+        configureMiniPlayerLook()
+    }
+    
+    private func configureMiniPlayerLook() {
+        artwork.layer.cornerRadius = 8.0
+        remainTime.isHidden = true
+        loopButton.isHidden = true
+        shuffleButton.isHidden = true
+        rewindButton.isHidden = true
+        fastForwardButton.isHidden = true
+        volumeSlider.isHidden = true
+        
         progressView.snp.makeConstraints { make in
             make.top.equalTo(snp.top)
             make.left.equalTo(snp.left)
             make.right.equalTo(snp.right)
-            make.height.equalTo(4)
+            make.height.equalTo(Constant.progressHeight)
         }
         
         artwork.snp.makeConstraints { make in
@@ -90,23 +161,96 @@ final class PlayerView: UIView {
             make.height.equalTo(artwork.snp.width)
         }
         
-        title.snp.makeConstraints { make in
-            make.top.equalTo(progressView.snp.bottom).offset(15)
-            make.right.equalTo(artwork.snp.left).offset(-15)
-            make.left.equalTo(playControlButton.snp.right).offset(15)
-        }
-        
-        artistAndSong.snp.makeConstraints { make in
-            make.top.equalTo(title.snp.bottom)
-            make.left.equalTo(title.snp.left)
-            make.right.equalTo(title.snp.right)
-        }
-        
         playControlButton.snp.makeConstraints { make in
             make.left.equalTo(snp.left).offset(15)
             make.top.equalTo(snp.top).offset(15)
             make.width.equalTo(30)
             make.height.equalTo(playControlButton.snp.width)
         }
+        
+        remainTime.snp.makeConstraints { make in
+            make.right.equalTo(snp.right).offset(-10)
+            make.bottom.equalTo(progressView.snp.top)
+        }
+    }
+    
+    private func configurePlayerLook() {
+        artwork.layer.cornerRadius = 15.0
+        remainTime.isHidden = false
+        loopButton.isHidden = false
+        shuffleButton.isHidden = false
+        rewindButton.isHidden = false
+        fastForwardButton.isHidden = false
+        volumeSlider.isHidden = false
+
+        artwork.snp.makeConstraints { make in
+            make.top.equalTo(artistAndSong.snp.bottom).offset(15)
+            make.left.equalTo(snp.left).offset(15)
+            make.right.equalTo(snp.right).offset(-15)
+            make.height.equalTo(artwork.snp.width)
+        }
+        
+        playControlButton.snp.makeConstraints { make in
+            make.top.equalTo(artwork.snp.bottom).offset(50.0)
+            make.width.equalTo(30)
+            make.height.equalTo(playControlButton.snp.width)
+            make.centerX.equalTo(snp.centerX)
+        }
+        
+        rewindButton.snp.makeConstraints { make in
+            make.top.equalTo(playControlButton.snp.top)
+            make.width.equalTo(playControlButton.snp.width)
+            make.height.equalTo(rewindButton.snp.width)
+            make.right.equalTo(playControlButton.snp.left).offset(-20.0)
+        }
+        
+        loopButton.snp.makeConstraints { make in
+            make.top.equalTo(playControlButton.snp.top)
+            make.width.equalTo(playControlButton.snp.width)
+            make.height.equalTo(rewindButton.snp.width)
+            make.right.equalTo(rewindButton.snp.left).offset(-20.0)
+        }
+        
+        fastForwardButton.snp.makeConstraints { make in
+            make.top.equalTo(playControlButton.snp.top)
+            make.width.equalTo(playControlButton.snp.width)
+            make.height.equalTo(rewindButton.snp.width)
+            make.left.equalTo(playControlButton.snp.right).offset(20.0)
+        }
+        
+        shuffleButton.snp.makeConstraints { make in
+            make.top.equalTo(playControlButton.snp.top)
+            make.width.equalTo(playControlButton.snp.width)
+            make.height.equalTo(rewindButton.snp.width)
+            make.left.equalTo(fastForwardButton.snp.right).offset(20.0)
+        }
+        
+        progressView.snp.makeConstraints { make in
+            make.bottom.equalTo(snp.bottomMargin)
+            make.left.equalTo(snp.left)
+            make.right.equalTo(snp.right)
+            make.height.equalTo(4)
+        }
+        
+        volumeSlider.snp.makeConstraints { make in
+            make.left.equalTo(snp.left).offset(20)
+            make.right.equalTo(snp.right).offset(-20)
+            make.top.equalTo(playControlButton.snp.bottom).offset(20.0)
+            make.height.equalTo(30)
+        }
+    }
+    
+    func presentView() {
+        progressView.snp.removeConstraints()
+        artwork.snp.removeConstraints()
+        playControlButton.snp.removeConstraints()
+        configurePlayerLook()
+    }
+    
+    func didmissView() {
+        progressView.snp.removeConstraints()
+        artwork.snp.removeConstraints()
+        playControlButton.snp.removeConstraints()
+        configureMiniPlayerLook()
     }
 }
